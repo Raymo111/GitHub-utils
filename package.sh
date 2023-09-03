0b3834cdb97e28f619926608bd3ec1e1e048b72b
Raymond Li <hi@raymond.li>
2023-09-03 16:44:30 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-zip better-per-tab-zoom.zip background.js controller.js manifest.json LICENSE icon128.png popup.html styles.css README.md
+zip GitHub-utils.zip content.js manifest.json LICENSE icon128.png popup.html styles.css README.md
